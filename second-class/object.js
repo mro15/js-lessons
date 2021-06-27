924cc018c9fade384eefcbbfe870b0a4ead80a77
Marcela Ribeiro de Oliveira <marcela.ribeiro@alana.ai>
2021-06-26 23:29:29 -0300

    added second class exercise solution

---
+++ second-class/object.js
@@ -0,0 +1,19 @@
+meu_livro = {
+    'titulo': 'O leão, a feiticeira e o guarda-roupa',
+    'autor': 'C. S. Lewis',
+    'paginas': 180,
+    'lido': true,
+    'peso': 10.5,
+}
+
+console.log('Titulo: ', meu_livro.titulo)
+console.log('Autor: ', meu_livro.autor)
+console.log('Paginas: ', meu_livro.paginas)
+console.log('Lido: ', meu_livro.lido)
+console.log('Peso: ', meu_livro.peso)
+
+console.log('Titulo tem o tipo: ', typeof(meu_livro.titulo))
+console.log('Autor tem o tipo: ', typeof(meu_livro.autor))
+console.log('Paginas tem o tipo: ', typeof(meu_livro.paginas))
+console.log('Lido tem o tipo: ', typeof(meu_livro.lido))
+console.log('Peso tem o tipo: ', typeof(meu_livro.peso))
