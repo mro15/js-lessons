25ad0a86cbd1e5c28787029c10ec81b24acc1738
Marcela Ribeiro de Oliveira <marcela.ribeiro@alana.ai>
2021-07-12 23:23:32 -0300

    adicionado exemplo de switch

---
+++ third-class/fruteira.js
@@ -0,0 +1,39 @@
+function fruteira(fruta){
+
+    switch (fruta) {
+       case "banana":
+          console.log("Banana existe na fruteira");
+          break;
+       case "maçã":
+          console.log("Maçã existe na fruteira");
+          break;
+       case "manga":
+          console.log("Manga existe na fruteira");
+          break;
+       case "pêra":
+          console.log("Pêra existe na fruteira");
+          break;
+       case "pêssego":
+          console.log("Pêssego existe na fruteira");
+           break;
+       default:
+          console.log("A fruta " + fruta + " não existe na fruteira");
+    }
+}
+
+fruta = "banana";
+fruteira(fruta);
+fruta = "maçã";
+fruteira(fruta);
+fruta = "manga";
+fruteira(fruta);
+fruta = "pêra";
+fruteira(fruta);
+fruta = "pêssego";
+fruteira(fruta);
+fruta = "laranja";
+fruteira(fruta);
+fruta = "morango";
+fruteira(fruta);
+fruta = "melão";
+fruteira(fruta);
