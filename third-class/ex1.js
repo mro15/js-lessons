e97f3a1d3edffb8f93ea65ff9164a607b1315bb5
Marcela Ribeiro de Oliveira <marcela.ribeiro@alana.ai>
2021-07-18 19:16:42 -0300

    resolution ex3.1

---
+++ third-class/ex1.js
@@ -0,0 +1,18 @@
+// Exercicio triangulos
+function triangulo(lado1, lado2, lado3){
+    if ( (lado1==lado2) && (lado1==lado3)){
+        console.log("equilatero");
+    }else if ((lado1!==lado2) && (lado2!==lado3) && (lado1!==lado3)){
+        console.log("escaleno");
+    }else{
+        console.log("isosceles");
+    }
+}
+
+lado1 = 1, lado2 = 1, lado3 = 2;
+triangulo(lado1, lado2, lado3);
+
+lado1 = 1, lado2 = 1, lado3 = 1;
+triangulo(lado1, lado2, lado3);
+lado1 = 1, lado2 = 3, lado3 = 2;
+triangulo(lado1, lado2, lado3);
